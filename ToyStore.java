f96ef0643e2efb81157f3b89dbe2203e2c6da6ac
NunkyIroh <vadegtyarev@gamai.com>
2024-03-08 16:49:07 +0300

    Добавлен класс Toy и поля класса

--- ToyStore.java
+++ ToyStore.java
@@ -3,3 +3,9 @@ import java.io.IOException;
 import java.util.PriorityQueue;
 import java.util.Random;
 
+class Toy implements Comparable<Toy> {
+    private int id;
+    private String name;
+    private int weight;
+
+}
