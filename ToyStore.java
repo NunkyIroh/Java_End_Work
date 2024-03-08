68e05c4a16eb51cf1ea3e7c84577bbd6ab095e95
NunkyIroh <vadegtyarev@gamai.com>
2024-03-08 16:59:44 +0300

    Добавлен класс ToyStore и метод getRandomToy

--- ToyStore.java
+++ ToyStore.java
@@ -3,6 +3,26 @@ import java.io.IOException;
 import java.util.PriorityQueue;
 import java.util.Random;
 
+public class ToyStore {
+
+    private static Toy getRandomToy(PriorityQueue<Toy> toyQueue, Random random) {
+        int totalWeight = 0;
+        for (Toy toy : toyQueue) {
+            totalWeight += toy.getWeight();
+        }
+
+        int randomNumber = random.nextInt(totalWeight);
+        int cumulativeWeight = 0;
+        for (Toy toy : toyQueue) {
+            cumulativeWeight += toy.getWeight();
+            if (randomNumber < cumulativeWeight) {
+                return toy;
+            }
+        }
+        return null;
+    }
+}
+
 class Toy implements Comparable<Toy> {
     private int id;
     private String name;
@@ -13,6 +33,7 @@ class Toy implements Comparable<Toy> {
         this.name = name;
         this.weight = weight;
     }
+
     public int getId() {
         return id;
     }
@@ -24,7 +45,7 @@ class Toy implements Comparable<Toy> {
     public int getWeight() {
         return weight;
     }
-    
+
     @Override
     public int compareTo(Toy other) {
         return Integer.compare(weight, other.weight);
